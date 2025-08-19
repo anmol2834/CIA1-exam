13aa4df38ccf8fd554066e53eef2aaa466d846c6
Virajsinh <jadavvirajsinhji@gmail.com>
2025-08-19 15:52:32 +0530

    fetched api

--- src/app/dashboard/profile/page.js
+++ src/app/dashboard/profile/page.js
@@ -1,27 +1,28 @@
-
-
-
-import React from 'react'
+import React from "react";
 
 function page() {
-//  Create a Next.js application that fetches a list of users from an API. Each user’s name should link to a dynamic route based on their username. On visiting /[username], display that user’s website. If the user is not found, then add a 404 default page.
+  //  Create a Next.js application that fetches a list of users from an API. Each user’s name should link to a dynamic route based on their username. On visiting /[username], display that user’s website. If the user is not found, then add a 404 default page.
 
-const API_URL = 'https://jsonplaceholder.typicode.com/users';
+  const API_URL = "https://jsonplaceholder.typicode.com/users";
 
-const fetchUsers = async () => {
-  const response = await fetch(API_URL);
+  let data;
+
+  async function fetchUsers() {
+    const response = await fetch(API_URL);
     if (!response.ok) {
-        throw new Error('Failed to fetch users');
+      throw new Error("Failed to fetch users");
     }
-    const data = await response.json();
-    return data;
-}
+    data = await response.json();
+    console.log(data);
+  }
 
-console.log(fetchUsers());
+  fetchUsers();
 
   return (
-    <div className='page-container'>profile page</div>
-  )
+    <div className="page-container">
+      profile page
+    </div>
+  );
 }
 
-export default page
+export default page;
